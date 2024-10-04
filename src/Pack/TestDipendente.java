2b2dde366b83dcc715a9da098c5728ea7b8b9db0
NikiValle <valle.nicola.studente@itispaleocapa.it>
2024-10-04 09:47:29 +0200

    Versoine array

--- src/Pack/TestDipendente.java
+++ src/Pack/TestDipendente.java
@@ -2,9 +2,13 @@ package Pack;
 
 public class TestDipendente {
     public static void main(String[]args){
-        Dipendente Mike = new Dipendente("00309", 1000.0,7.5);
-        System.out.println("Stipendio: "+Mike.getStipendio());
-        System.out.println("Pagato: "+Mike.paga(16));
-        Mike.stampa();
+        Dipendente[] pepe = new Dipendente[5];
+        pepe[0] = new Dipendente("00309", 1000.0,7.5);
+        for(int i=0;i<5;i++) {
+            pepe[i] = new Dipendente("00309", 1000.0+(i*10),7.5+(i+2.5));
+            System.out.println("Stipendio: " + pepe[i].getStipendio());
+            System.out.println("Pagato: " + pepe[i].paga(16));
+            pepe[i].stampa();
+        }
     }
 }
