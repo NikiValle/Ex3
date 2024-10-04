2ffa181b6c41738603857b50a45f9fec4af3d73a
NikiValle <valle.nicola.studente@itispaleocapa.it>
2024-10-04 09:48:36 +0200

    Versoione array sistemats

--- src/Pack/TestDipendente.java
+++ src/Pack/TestDipendente.java
@@ -2,13 +2,13 @@ package Pack;
 
 public class TestDipendente {
     public static void main(String[]args){
-        Dipendente[] pepe = new Dipendente[5];
-        pepe[0] = new Dipendente("00309", 1000.0,7.5);
+        Dipendente[] dipendenti = new Dipendente[5];
+        dipendenti[0] = new Dipendente("00309", 1000.0,7.5);
         for(int i=0;i<5;i++) {
-            pepe[i] = new Dipendente("00309", 1000.0+(i*10),7.5+(i+2.5));
-            System.out.println("Stipendio: " + pepe[i].getStipendio());
-            System.out.println("Pagato: " + pepe[i].paga(16));
-            pepe[i].stampa();
+            dipendenti[i] = new Dipendente("00309", 1000.0+(i*10),7.5+(i+2.5));
+            System.out.println("Stipendio: " + dipendenti[i].getStipendio());
+            System.out.println("Pagato: " + dipendenti[i].paga(16));
+            dipendenti[i].stampa();
         }
     }
 }
